308e66303a0ed6ae1f792b783a3db8ac43328b92
Nemeth Zsolt <furion1@gmail.com>
2016-08-27 11:59:34 -0400

    share menu feed and list

--- src/components/FeedItem.vue
+++ src/components/FeedItem.vue
@@ -11,9 +11,9 @@
           <a class="host" href="{{item.link}}">{{item.feed}}</a>
           <span class="diff">{{item.diff}} ago</span>
           <span class="share general icon-share" v-bind:class="{active: isShareActive}" @click="toggleShare"></span>
-          <a href="https://twitter.com/intent/tweet?original_referer=http://curatist.co&text={{item.title}}&tw_p=tweetbutton&url={{item.link}}&via=The_Curatist" class="share twitter icon-twitter-with-circle" v-show="isShareActive"></a>
-          <a href="https://www.facebook.com/sharer/sharer.php?u={{item.link}}" class="popup share facebook icon-facebook-with-circle" v-show="isShareActive"></a>
-          <a href="http://www.linkedin.com/shareArticle?mini=true&url={{item.link}}&title={{item.title}}&summary={{item.summary}}&source={{item.feed}}" class="share linkedin icon-linkedin-with-circle" v-show="isShareActive"></a>
+          <a href="http://www.linkedin.com/shareArticle?mini=true&url={{item.link}}&title={{item.title}}&summary={{item.summary}}&source={{item.feed}}" class="share linkedin icon-linkedin-with-circle" ></a>
+          <a href="https://www.facebook.com/sharer/sharer.php?u={{item.link}}" class="popup share facebook icon-facebook-with-circle"></a>
+          <a href="https://twitter.com/intent/tweet?original_referer=http://curatist.co&text={{item.title}}&tw_p=tweetbutton&url={{item.link}}&via=The_Curatist" class="share twitter icon-twitter-with-circle"></a>
         </div>
       </header>
   </div>
@@ -50,6 +50,38 @@
 </script>
 
 <style lang="sass">
+  .bounce-transition {
+    display: inline-block; /* otherwise scale animation won't work */
+  }
+  .bounce-enter {
+    animation: bounce-in .5s;
+  }
+  .bounce-leave {
+    animation: bounce-out .5s;
+  }
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+  @keyframes bounce-out {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(0);
+    }
+  }
+
 
  @import "../styles/feeditem.scss";
 </style>
